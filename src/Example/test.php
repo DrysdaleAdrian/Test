62f2165b0905f15359dc363f09e174b7853f025b
Adrian Drysdale <adrian.drysdale@visualsoft.co.uk>
2019-03-02 20:51:37 +0000

    test commit

---
+++ src/Example/test.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Example;
+
+/**
+ * Class test
+ *
+ * @package Example
+ */
+class test
+{
+
+    /**
+     *
+     *
+     * @return int
+     *
+     * @author Adrian Drysdale
+     */
+    public function power(): int
+    {
+
+        return 5 * 2;
+    }
+}
